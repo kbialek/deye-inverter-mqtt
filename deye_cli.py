68cb9ecb91b05810297531992e484e386eabefd0
Krzysztof Białek <kbialek@users.noreply.github.com>
2022-09-13 22:17:31 +0200

    Add license headers

--- deye_cli.py
+++ deye_cli.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 import logging
 import sys
 
@@ -16,6 +33,7 @@ log = logging.getLogger('main')
 connector = DeyeConnector(config)
 modbus = DeyeModbus(config, connector)
 
+
 def read_register(args):
     reg_address = int(args[0])
     registers = modbus.read_registers(reg_address, reg_address)
@@ -27,16 +45,18 @@ def read_register(args):
         sys.exit(1)
     print(int.from_bytes(registers[reg_address], 'big'))
 
+
 def write_register(args):
     if len(args) < 2:
         print("Not enough arguments")
-        sys.exit(1) 
+        sys.exit(1)
     reg_address = int(args[0])
     reg_value = int(args[1])
     if modbus.write_register(reg_address, reg_value):
         print("Ok")
     else:
         print("Error")
+
 
 def main():
     args = sys.argv[1:]
@@ -45,6 +65,7 @@ def main():
         read_register(args[1:])
     elif command == 'w':
         write_register(args[1:])
+
 
 if __name__ == "__main__":
     main()
