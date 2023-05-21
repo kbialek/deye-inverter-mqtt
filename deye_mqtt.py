2b6b8c9a235f2e9f5f456180182610f9a4cf58a9
matthias <palto@mailbox.org>
2023-05-21 08:26:08 +0000

    Added log message for TLS mode

--- deye_mqtt.py
+++ deye_mqtt.py
@@ -36,12 +36,16 @@ class DeyeMqttClient():
             if config.mqtt.tls.insecure:
                 self.__mqtt_client.tls_set(cert_reqs=ssl.CERT_NONE)
                 self.__mqtt_client.tls_insecure_set(True)
+                self.__log.info(
+                    "Enabled TLS encryption for MQTT Broker connection without certificate verification (insecure)"
+                )
             else:
                 self.__mqtt_client.tls_set(
                     ca_certs=config.mqtt.tls.ca_cert_path,
                     certfile=config.mqtt.tls.client_cert_path,
                     keyfile=config.mqtt.tls.client_key_path
                 )
+                self.__log.info("Enabled TLS encryption for MQTT Broker connection with certificate verification")
         if config.mqtt.username and config.mqtt.password:
             self.__mqtt_client.username_pw_set(username=config.mqtt.username, password=config.mqtt.password)
         self.__status_topic = f'{config.mqtt.topic_prefix}/{config.mqtt.availability_topic}'
