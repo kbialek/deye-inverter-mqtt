01df1653665c32f84cccbf879ae820e3e885e7d7
Krzysztof Białek <kbialek@users.noreply.github.com>
2024-02-05 21:12:17 +0100

    Move data reading into DeyeInverterState class
    Signed-off-by: Krzysztof Białek <kbialek@users.noreply.github.com>

--- src/deye_daemon.py
+++ src/deye_daemon.py
@@ -23,49 +23,44 @@ import time
 
 from deye_config import DeyeConfig
 from deye_connector_factory import DeyeConnectorFactory
-from deye_events import DeyeEventList, DeyeLoggerStatusEvent, DeyeObservationEvent
+from deye_events import DeyeEventList, DeyeLoggerStatusEvent, DeyeObservationEvent, DeyeEventProcessor
 from deye_modbus import DeyeModbus
 from deye_mqtt import DeyeMqttClient
 from deye_observation import Observation
-from deye_sensor import SensorRegisterRanges
+from deye_sensor import SensorRegisterRanges, Sensor
 from deye_sensors import sensor_list, sensor_register_ranges
 from deye_processor_factory import DeyeProcessorFactory
 
 
-class DeyeDaemon:
-    def __init__(self, config: DeyeConfig):
-        self.__log = logging.getLogger(DeyeDaemon.__name__)
+class DeyeInverterState:
+    def __init__(
+        self,
+        config: DeyeConfig,
+        reg_ranges: SensorRegisterRanges,
+        modbus: DeyeModbus,
+        sensors: list[Sensor],
+        processors: list[DeyeEventProcessor],
+    ):
+        self.__log = logging.getLogger(DeyeInverterState.__name__)
         self.__config = config
-        self.__log.info(
-            "Please help me build the list of compatible inverters. "
-            "https://github.com/kbialek/deye-inverter-mqtt/issues/41"
-        )
-
-        connector = DeyeConnectorFactory(config).create_connector()
-        self.modbus = DeyeModbus(connector)
-        self.sensors = [s for s in sensor_list if s.in_any_group(self.__config.metric_groups)]
-        self.reg_ranges = SensorRegisterRanges(
-            sensor_register_ranges, config.metric_groups, max_range_length=config.logger.max_register_range_length
-        ).ranges
-
-        mqtt_client = DeyeMqttClient(self.__config)
-
-        self.processors = DeyeProcessorFactory(self.__config, mqtt_client).create_processors(self.modbus, self.sensors)
-
+        self.__reg_ranges = reg_ranges
+        self.__modbus = modbus
+        self.__sensors = sensors
+        self.__processors = processors
         self.__last_observations = DeyeEventList()
         self.__event_updated = time.time()
 
-    def do_task(self):
+    def read_from_logger(self):
         self.__log.info("Reading start")
         regs = {}
-        for reg_range in self.reg_ranges:
+        for reg_range in self.__reg_ranges.ranges:
             self.__log.info(f"Reading registers [{reg_range}]")
-            regs |= self.modbus.read_registers(reg_range.first_reg_address, reg_range.last_reg_address)
+            regs |= self.__modbus.read_registers(reg_range.first_reg_address, reg_range.last_reg_address)
         events = DeyeEventList()
         events.append(DeyeLoggerStatusEvent(len(regs) > 0))
         events += self.__get_observations_from_reg_values(regs)
         if not self.__config.publish_on_change or self.__is_device_observation_changed(events):
-            for processor in self.processors:
+            for processor in self.__processors:
                 processor.process(events)
         else:
             self.__log.info("No changes found in received data, or the logger is offline, skipping")
@@ -74,7 +69,7 @@ class DeyeDaemon:
     def __get_observations_from_reg_values(self, regs: dict[int, bytearray]) -> list[DeyeObservationEvent]:
         timestamp = datetime.datetime.now()
         events = []
-        for sensor in self.sensors:
+        for sensor in self.__sensors:
             value = sensor.read_value(regs)
             if value is not None:
                 observation = Observation(sensor, timestamp, value)
@@ -110,6 +105,31 @@ class DeyeDaemon:
         self.__event_updated = time.time()
         self.__last_observations = events
         return True
+
+
+class DeyeDaemon:
+    def __init__(self, config: DeyeConfig):
+        self.__log = logging.getLogger(DeyeDaemon.__name__)
+        self.__config = config
+        self.__log.info(
+            "Please help me build the list of compatible inverters. "
+            "https://github.com/kbialek/deye-inverter-mqtt/issues/41"
+        )
+
+        connector = DeyeConnectorFactory(config).create_connector()
+        modbus = DeyeModbus(connector)
+        sensors = [s for s in sensor_list if s.in_any_group(self.__config.metric_groups)]
+        reg_ranges = SensorRegisterRanges(
+            sensor_register_ranges, config.metric_groups, max_range_length=config.logger.max_register_range_length
+        )
+
+        mqtt_client = DeyeMqttClient(self.__config)
+
+        processors = DeyeProcessorFactory(self.__config, mqtt_client).create_processors(modbus, sensors)
+        self.__inverter_state = DeyeInverterState(config, reg_ranges, modbus, sensors, processors)
+
+    def do_task(self):
+        self.__inverter_state.read_from_logger()
 
 
 class IntervalRunner:
